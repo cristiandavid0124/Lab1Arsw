43a1cd55d1a3791472d15c0ff769340e311cbeec
unknown <diego.castellanos-a@labinfo.is.escuelaing.edu.co>
2024-08-17 12:11:59 -0500

    2do punto completo(codigo), falta readme

--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -5,14 +5,25 @@
  */
 package edu.eci.arsw.threads;
 
+import   java.lang.*;
+
 /**
  *
  * @author hcadavid
  */
-public class CountThreadsMain {
-    
-    public static void main(String a[]){
-        
+public class CountThreadsMain implements Runnable{
+
+    public void run(){
+
     }
     
+    public static void main(String[] args){
+        Thread thread1 =  new Thread(new CountThread(0, 99));
+        Thread thread2 =  new Thread(new CountThread(99, 199));
+        Thread thread3 =  new Thread(new CountThread(200, 299));
+        thread1.run();
+        thread2.run();
+        thread3.run();
+    }
+
 }
