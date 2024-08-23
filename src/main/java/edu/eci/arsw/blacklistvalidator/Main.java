6b7b2689a86b842cfc46f6cc60bbc6e3918768fc
Diegoc04 <diego.fercho.castellanos@gmail.com>
2024-08-23 18:40:48 -0500

    Terminado el Lab

--- src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
+++ src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
@@ -7,15 +7,10 @@ package edu.eci.arsw.blacklistvalidator;
 
 import java.util.List;
 
-
-
-import java.util.List;
-
 public class Main {
-
+    
     public static void main(String[] args) {
         int cores = Runtime.getRuntime().availableProcessors();
-        // Ejecutar los experimentos
         //runExperiment("202.24.34.55", 1);
         //runExperiment("202.24.34.55", cores);
         //runExperiment("202.24.34.55", cores * 2);
@@ -25,8 +20,10 @@ public class Main {
 
     private static void runExperiment(String ipaddress, int threads) {
         HostBlackListsValidator hblv = new HostBlackListsValidator();
-        List<Integer> blackListOcurrences = hblv.checkHost(" 212.24.24.55", 10);
-
+        long startTime = System.currentTimeMillis();
+        List<Integer> blackListOcurrences = hblv.checkHost(ipaddress, threads);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Execution time with " + threads + " threads: " + (endTime - startTime) + "ms");
         System.out.println("The host was found in the following blacklists:" + blackListOcurrences);
     }
 }
